9bbcecaf7559804f6e4afa676e9ffe9c050a61a5
Alyssa <alyssa.brooksong@gmail.com>
2023-06-22 14:50:38 -0700

    with help from Lantz

--- notes.md
+++ notes.md
@@ -1,9 +1,36 @@
+##Core Deliverables
+
 As a user, I can:
 
-1. See all ramen images in the `div` with the id of `ramen-menu`. When the page loads, request the data from the server to get all the ramen objects. Then, display the image for each of the ramen using an `img` tag inside the
-  `#ramen-menu` div.
-  [] 
-2. Click on an image from the `#ramen-menu` div and see all the info about that ramen displayed inside the `#ramen-detail` div and where it says `insert comment here` and `insert rating here`.
-[] create eventListener for click on image, have info display
+1. See all ramen images in the `div` with the id of `ramen-menu`. When the page loads, request the data from the server to get all the ramen objects. Then, display the image for each of the ramen using an `img` tag inside the `#ramen-menu` div.
+  See all ramen image in menu on page load
+    [X] fetch ramen data from server
+    [X] iterate the ramen array (forEach)
+    [X] create <image> element for each ramen
+    [X] assign the src to each <img> from the ramen obj
+    [X] select menu element from DOM (anchor)
+    [X] append <img> to menu
 
-3. Create a new ramen after submitting the `new-ramen` form. The new ramen should be added to the`#ramen-menu` div. The new ramen does not need to persist; in other words, if you refresh the page, it's okay that the new ramen is no longer on the page. 
+
+2. Click on an menu image to see details of that ramen shows in detai section
+    [X] add event listeners to <img> in menu
+    [X] handle events by calling a detail render function
+    [X] select appropriate elements
+    [X] assign data to those elements from ramen obj
+
+
+3. Submit a form to create a new ramen in the menu(no persistence) - just open in DOM
+    [] select the form
+    [] add event listener to form
+    [] handle submit event with callback function
+    [] preventDefault() for form refresh
+    [] get value from each input (via the event OR selector)
+    [] create new ramenObj with values from form
+    [] call render function passing new ramenObj
+
+##Data Shape
+
+##End points
+baseURL: http://localhost:3000
+GET /ramens
+Get /ramens/:id
--- src/index.js
+++ src/index.js
@@ -1,33 +1,65 @@
 // write your code here
 
 //Global Scope
-  const URL = "http://locahost:3000/ramens"
+  const baseURL = "http://localhost:3000"
 
 //DOM Selectors
-const detailImage = document.querySelector("#ramen-detail")
-const owner = document.querySelector("#name")
-const restaurant = document.querySelector("#restaurant")
-
 const menu = document.querySelector("#ramen-menu")
+const detailImage = document.querySelector(".detail-image")
+const ramenName = document.querySelector(".name")
+const restaurant = document.querySelector(".restaurant")
+
 const ratingDisplay = document.querySelector("#rating-display")
 const commentDisplay = document.querySelector("#comment-display")
+const newRamen = document.querySelector("#new-ramen")
+
 
 //Fetch Functions
 function getRamens(url){
-    fetch("http://locahost:3000/ramens")
+    return fetch(url) 
         .then(response => response.json())
 }
 
 //Render Functions
-function renderRamens(data) {
-    const ramens = data
-    detailImage.src = ramens.image
-    owner.textContent = ramens.name
-    restaurant.textContent = ramens.restaurant
-    menu.src = ramens.image
-    ratingDisplay.textContent = ramens.rating
-    commentDisplay = ramens.comment
+function renderAllRamen(ramenArr){
+    ramenArr.forEach(renderInMenu)
+}
+
+function renderInMenu(ramenObj) {
+    const img = document.createElement("img");
+    img.src = ramenObj.image;
+    img.addEventListener("click", () => renderRamenDetail(ramenObj));
+    menu.append(img);
+
+}
+function renderRamenDetail(ramenObj) {
+    detailImage.src = ramenObj.image
+    ramenName.textContent = ramenObj.name
+    restaurant.textContent = ramenObj.restaurant
+    ratingDisplay.textContent = ramenObj.rating
+    commentDisplay.textContent = ramenObj.comment
 }
 
 
 //Event Listener
+newRamen.addEventListener('submit', handleNewRamen)
+
+//Event handlers
+function handleNewRamen(e){
+    e.preventDefault();
+    const newRamen = {
+        name: e.target.name.value,
+        restuarant: e.target.restaurant.value,
+        image: e.target.image.value,
+        raiting: e.target.rating.value,
+        comment: e.target["new-comment"],
+    }
+    renderInMenu(newRamen)
+}
+
+//Initializers
+getRamens(baseURL + '/ramens')
+    .then(ramenArr => {
+        //ramenArr.ForEach(renderInMenu)
+        renderAllRamen(ramenArr)
+    })
