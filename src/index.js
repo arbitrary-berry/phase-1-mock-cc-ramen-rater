a5c90c11eafa7fa08b03fa936643b9ff749b81f1
Alyssa <alyssa.brooksong@gmail.com>
2023-06-22 12:14:04 -0700

    end of 90 mins

--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
 // write your code here
+
+//Global Scope
+  const URL = "http://locahost:3000/ramens"
+
+//DOM Selectors
+const detailImage = document.querySelector("#ramen-detail")
+const owner = document.querySelector("#name")
+const restaurant = document.querySelector("#restaurant")
+
+const menu = document.querySelector("#ramen-menu")
+const ratingDisplay = document.querySelector("#rating-display")
+const commentDisplay = document.querySelector("#comment-display")
+
+//Fetch Functions
+function getRamens(url){
+    fetch("http://locahost:3000/ramens")
+        .then(response => response.json())
+}
+
+//Render Functions
+function renderRamens(data) {
+    const ramens = data
+    detailImage.src = ramens.image
+    owner.textContent = ramens.name
+    restaurant.textContent = ramens.restaurant
+    menu.src = ramens.image
+    ratingDisplay.textContent = ramens.rating
+    commentDisplay = ramens.comment
+}
+
+
+//Event Listener
